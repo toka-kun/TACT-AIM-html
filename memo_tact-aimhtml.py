7fb6b9d77e54caf99845afa8d3a85b35a042894e
monnku <129637317+monnku@users.noreply.github.com>
2024-10-13 15:14:40 +0900

    Add files via upload

---
+++ memo_tact-aimhtml.py
@@ -0,0 +1,16 @@
+"""
+<h1 class="loading-text">Credit: @uk-400様</h1>
+と調べて
+    <h2 class="username" onclick="
+    window.user = window.prompt('ユーザー名'); scaffolding.setUsername(window.user);
+    ">ユーザー名変更</h2>
+をその下にする
+cssの所らへんに
+    .username{
+      background-color: grey;
+      height: 40px;
+      text-align: center;
+      cursor: pointer;
+    }
+これを追加する
+"""
